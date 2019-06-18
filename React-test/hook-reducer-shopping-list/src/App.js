20cddd2935ec1d949e365e28a25120f1d313c07b
Saeid Pahlevan <s.pahlevan@fugro.com>
2019-06-18 09:38:53 +0800

    reducer shopping list

---
+++ React-test/hook-reducer-shopping-list/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ShoppingList } from "./shoppingList";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ShoppingList />
+    </div>
+  );
+}
+
+export default App;
