b9524c551af758a4ae2bfd1a4bba69c96311d2ff
Saeid Pahlevan <s.pahlevan@fugro.com>
2019-07-15 08:54:55 +0800

    fetch using loading indicator

--- React-test/react-fetch/src/App.js
+++ React-test/react-fetch/src/App.js
@@ -1,27 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
   const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState("redux");
+  const [url, setUrl] = useState(
+    "http://hn.algolia.com/api/v1/search?query=redux"
+  );
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(
-        "http://hn.algolia.com/api/v1/search?query=redux"
-      );
+      setIsLoading(true);
+      const result = await axios(url);
       setData(result.data);
-    }
+      setIsLoading(false);
+    };
     fetchData();
-  }, []);
+  }, [url]);
 
   return (
-    <ul>
-      {data.hits.map(item => (
-        <li key={item.objectID}>
-          <a href={item.url}>{item.title}</a>
-        </li>
-      ))}
-    </ul>
+    <Fragment>
+      <input
+        type="text"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      />
+      <button
+        type="button"
+        onClick={() =>
+          setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`)
+        }
+      >
+        Search
+      </button>
+
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <ul>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </Fragment>
   );
 }
 
