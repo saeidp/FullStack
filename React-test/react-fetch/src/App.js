7f7d3ea0f465d367df4a32ecd821da4a985cef3e
Saeid <saeidpahlevan@hotmail.com>
2019-07-16 06:21:09 +0800

    hooks for fetching data

--- React-test/react-fetch/src/App.js
+++ React-test/react-fetch/src/App.js
@@ -1,39 +1,59 @@
 import React, { Fragment, useState, useEffect } from "react";
 import axios from "axios";
 
-function App() {
-  const [data, setData] = useState({ hits: [] });
-  const [query, setQuery] = useState("redux");
-  const [url, setUrl] = useState(
-    "http://hn.algolia.com/api/v1/search?query=redux"
-  );
+const useDataApi = (initialUrl, initialData) => {
+  const [data, setData] = useState(initialData);
+  const [url, setUrl] = useState(initialUrl);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsError(false);
       setIsLoading(true);
-      const result = await axios(url);
-      setData(result.data);
+
+      try {
+        const result = await axios(url);
+
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+
       setIsLoading(false);
     };
+
     fetchData();
   }, [url]);
 
+  return [{ data, isLoading, isError }, setUrl];
+};
+
+function App() {
+  const [query, setQuery] = useState("redux");
+  const [{ data, isLoading, isError }, doFetch] = useDataApi(
+    "http://hn.algolia.com/api/v1/search?query=redux",
+    { hits: [] }
+  );
+
   return (
     <Fragment>
-      <input
-        type="text"
-        value={query}
-        onChange={event => setQuery(event.target.value)}
-      />
-      <button
-        type="button"
-        onClick={() =>
-          setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`)
-        }
+      <form
+        onSubmit={event => {
+          doFetch(`http://hn.algolia.com/api/v1/search?query=${query}`);
+
+          event.preventDefault();
+        }}
       >
-        Search
-      </button>
+        <input
+          type="text"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {isError && <div>Something went wrong ...</div>}
 
       {isLoading ? (
         <div>Loading ...</div>
