ec7e62fa4955c3db136834a8fc4723e2dd0ea297
Saeid <saeidpahlevan@hotmail.com>
2019-07-16 19:39:21 +0800

    Added reducer for fetching data

--- React-test/react-fetch/src/App.js
+++ React-test/react-fetch/src/App.js
@@ -1,32 +1,65 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect, useReducer } from "react";
 import axios from "axios";
 
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_INIT":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false
+      };
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload
+      };
+    case "FETCH_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true
+      };
+    default:
+      throw new Error();
+  }
+};
 const useDataApi = (initialUrl, initialData) => {
-  const [data, setData] = useState(initialData);
   const [url, setUrl] = useState(initialUrl);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
+
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData
+  });
 
   useEffect(() => {
+    let didCancel = false;
     const fetchData = async () => {
-      setIsError(false);
-      setIsLoading(true);
-
+      dispatch({ type: "FETCH_INIT" });
       try {
         const result = await axios(url);
-
-        setData(result.data);
+        if (!didCancel) {
+          dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+        }
       } catch (error) {
-        setIsError(true);
+        if (!didCancel) {
+          dispatch({ type: "FETCH_FAILURE" });
+        }
       }
-
-      setIsLoading(false);
     };
 
     fetchData();
+    //didCancel will prevent setting component state if
+    // already unmounted
+    return () => {
+      didCancel = true;
+    };
   }, [url]);
 
-  return [{ data, isLoading, isError }, setUrl];
+  return [state, setUrl];
 };
 
 function App() {
@@ -35,7 +68,6 @@ function App() {
     "http://hn.algolia.com/api/v1/search?query=redux",
     { hits: [] }
   );
-
   return (
     <Fragment>
       <form
