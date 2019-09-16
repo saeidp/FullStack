f80ac7c0e6dfe3acadc661dc8c9d44d49cd7503e
Saeid <saeidpahlevan@hotmail.com>
2019-09-16 19:09:17 +0800

    asyn await generators and promise

--- TypeScript/deepDive.js
+++ TypeScript/deepDive.js
@@ -1,367 +1,126 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 exports.__esModule = true;
-// interface Point2D {
-//   x: number;
-//   y: number;
+var status = "Bad";
+// you get intellisense and you are just able to assign one of those values
+//--------------------------------------------------
+// //Promise
+// function iReturnPromiseAfter1Second(): Promise<string> {
+//   return new Promise(resolve => {
+//     setTimeout(() => resolve("Hello world!"), 1000);
+//   });
 // }
-// var point2D: Point2D = { x: 0, y: 10 };
-// console.log(point2D);
-// -------------------------------------------------------
-// Jquey $
-// // declare var $: any;
-// // $('.awesome').show();
-// ------------------------------------------------------
-// class Point {
-//   constructor(public x: number, public y: number) {}
-//   add(point: Point) {
-//     return new Point(this.x + point.x, this.y + point.y);
-//   }
-// }
-// var p1 = new Point(0, 10);
-// var p2 = new Point(10, 20);
-// var p3 = p1.add(p2); // { x: 10, y: 30 }
-// console.log(p3);
-// --------------------------------------------------------
-// type Idisplay = {
-//   id: string;
-//   display: string;
-// };
-// const list: Idisplay[] = [
-//   { id: "foo", display: "foo select" },
-//   { id: "bar", display: "bar select" }
-// ];
-// const fooIndex = list.map(i => i.id).indexOf("bar");
-// console.log(fooIndex);
-// --------------------------------------------------------
-// Return an object
-// function toInt(str: string): { valid: boolean; int?: number } {
-//   const int = parseInt(str);
-//   if (isNaN(int)) {
-//     return { valid: false };
-//   } else {
-//     return { valid: true, int };
-//   }
-// }
-// const { valid, int } = toInt("s10");
-// const { valid, int } = toInt("10");
-// if (valid) {
-//   console.log(int);
-// }
-// const obj = toInt("10");
-// if (obj.valid) {
-//   console.log(obj.int);
-// }
-// ---------------------------------------------------------------
-// undefined is not used in json
-// console.log(JSON.stringify({ willStay: null, willBeGone: undefined })); // {"willStay":null}
-// ------------------------------------------------------------------------------------------------
-// this
-// Any access to this keyword within a function is actually controlled
-// by how the function is actually called.It is commonly referred to as
-// the “calling context.”
-// function foo() {
-//   console.log(this);
-// }
-// foo(); // logs out the global e.g. `window` in browsers
-// let bar = {
-//   foo
-// };
-// bar.foo(); // Logs out `bar` as `foo` was called on `bar`
-// ------------------------------------------------------------------------
-// Closures
-// function in JavaScript has access to any variables defined in the
-// outer scope
-// function outerFunction(arg) {
-//     var variableInOuterFunction = arg;
-//     function bar() {
-//         console.log(variableInOuterFunction); // Access a variable from the outer scope
-//     }
-//     // Call the local function to demonstrate that it has access to arg
-//     bar();
-// }
-// outerFunction("hello closure"); // logs hello closure!
-// // closures allow compose objects. the revealing module pattern:
-// function createCounter() {
-//     let val = 0;
-//     return {
-//         increment() { val++ },
-//         getVal() { return val }
-//     }
-// }
-// let counter = createCounter();
-// counter.increment();
-// console.log(counter.getVal()); // 1
-// counter.increment();
-// console.log(counter.getVal()); // 2
-// ------------------------------------------------------------------------------
-// Truthy
-// Being explicit on Truthy
-// let name1: string = "ss";
-// const hasName = !!name1;
-// console.log(hasName); // true
-// let name2: string = "";
-// const hasName2 = !!name2;
-// console.log(hasName2); // false
-// -------------------------------------------------------------
-// Inheritence
-// class Point {
-//   x: number;
-//   y: number;
-//   constructor(x: number, y: number) {
-//     this.x = x;
-//     this.y = y;
-//   }
-//   add(point: Point) {
-//     return new Point(this.x + point.x, this.y + point.y);
-//   }
-// }
-// class Point3D extends Point {
-//   z: number;
-//   constructor(x: number, y: number, z: number) {
-//     super(x, y);
-//     this.z = z;
-//   }
-//   add(point: Point3D) {
-//     var point2D = super.add(point);
-//     return new Point3D(point2D.x, point2D.y, this.z + point.z);
-//   }
-// }
-// ------------------------------------------------------------------
-// Arrow functions
-// var inc = (x: number) => x + 1;
-// this meaning is fixed using arrow function. This in below is windo in this case
-// this.age inside Person will not be read for growOld.
-// function Person(age) {
-//     this.age = age;
-//     this.growOld = function() {
-//         this.age++;
-//     }
-// }
-// var person = new Person(1);
-// setTimeout(person.growOld,1000);
-// setTimeout(function() { console.log(person.age); },2000); // 1, should have been 2
-// to fix the above we have to use arrow function
-// function Person(age) {
-//     this.age = age;
-//     this.growOld = () => {
-//         this.age++;
-//     }
-// }
-// var person = new Person(1);
-// setTimeout(person.growOld,1000);
-// setTimeout(function() { console.log(person.age); },2000); // 2
-// combine arrows with classes
-// class Person {
-//     constructor(public age:number) {}
-//     growOld = () => {
-//         this.age++;
-//     }
-// }
-// var person = new Person(1);
-// setTimeout(person.growOld,1000);
-// setTimeout(function() { console.log(person.age); },2000); // 2
-// ----------------------------------------------------------------------
-// Arrow functions and inheritance
-// class Adder {
-//     constructor(public a: number) {}
-//     add = (b: number): number => {
-//         return this.a + b;
-//     }
-// }
-// class Child extends Adder {
-//     callAdd(b: number) {
-//         return this.add(b);
-//     }
-// }
-// Demo to show it works
-// const child = new Child(123);
-// console.log(child.callAdd(123)); // 246
-// the above do not work with the super keyword when you try to
-//  override the function in a child class.
-// Since there is only one this such functions cannot participate in a call
-// to super (super only works on prototype members). You can easily get around it
-// by creating a copy of the method before overriding it in the child.
-// class Adder {
-//     constructor(public a: number) {}
-//     // This function is now safe to pass around
-//     add = (b: number): number => {
-//         return this.a + b;
-//     }
-// }
-// class ExtendedAdder extends Adder {
-//     // Create a copy of parent before creating our own
-//     private superAdd = this.add;
-//     // Now create our override
-//     add = (b: number): number => {
-//         return this.superAdd(b);
-//     }
-// }
-// ------------------------------------------------------------------------------
-// Quick object return
-// Sometimes you need a function that just returns a simple object literal
-// WRONG WAY TO DO IT. is parsed as a block containing a JavaScript Label by JavaScript runtimes
-// var foo = () => {
-//     bar: 123
-// };
-// You can fix it by surrounding the object literal with ()
-// Correct 🌹
-// var foo = () => ({
-//     bar: 123
+// const promise = new Promise((resolve, reject) => {
+//   resolve(123);
 // });
-// ------------------------------------------------------------------
-// Rest parameters
-// Rest parameters (denoted by ...argumentName for the last argument) allow you to quickly
-// accept multiple arguments in your function and get them as an array
-// function iTakeItAll(first, second, ...allOthers) {
-//     console.log(allOthers);
-// }
-// iTakeItAll('foo', 'bar'); // []
-// iTakeItAll('foo', 'bar', 'bas', 'qux'); // ['bas','qux']
-// ----------------------------------------------------------------------
-// let
-// var Variables in JavaScript are function scoped. This is different
-// from many other languages (C# / Java etc.) where the variables are block scoped
-// var foo = 123;
-// if (true) {
-//     var foo = 456;
-// }
-// console.log(foo); // 456
-// using let
-// let foo = 123;
-// if (true) {
-//     let foo = 456;
-// }
-// console.log(foo); // 123
-// This problem doesn't happen using function
-// functions create a new scope
-// var foo = 123;
-// function test() {
-//   console.log(foo); // undefined
-//   var foo = 456;
-// }
-// test();
-// console.log(foo); // 123
-// ---------------------------------------------------------------
-// let in closure
-// What is the output of this?
-// var funcs = [];
-// // create a bunch of functions
-// for (var i = 0; i < 3; i++) {
-//   funcs.push(function() {
-//     console.log(i);
+// promise
+//   .then(res => {
+//     // res is inferred to be of type `number`
+//     return iReturnPromiseAfter1Second();
+//   })
+//   .then(res => {
+//     // res is inferred to be of type `string`
+//     console.log(res); // Hello world!
 //   });
+//-------------------------------------------------------
+// Generators
+// function* infiniteSequence() {
+//   var i = 0;
+//   while (true) {
+//     yield i++;
+//   }
 // }
-// // call them
-// for (var j = 0; j < 3; j++) {
-//   funcs[j]();
+// var iterator = infiniteSequence();
+// while (true) {
+//   console.log(iterator.next()); // { value: xxxx, done: false } forever and ever
 // }
-// // output will be 3 3 3
-// // Use let to fix
-// var funcs = [];
-// // create a bunch of functions
-// for (let i = 0; i < 3; i++) {
-//   // Note the use of let
-//   funcs.push(function() {
-//     console.log(i);
-//   });
+// // Of course if the iterator does end, you get the result
+// // of { done: true } as demonstrated below:
+// function* idMaker() {
+//   let index = 0;
+//   while (index < 3) yield index++;
 // }
-// // call them
-// for (var j = 0; j < 3; j++) {
-//   funcs[j]();
+// let gen = idMaker();
+// console.log(gen.next()); // { value: 0, done: false }
+// console.log(gen.next()); // { value: 1, done: false }
+// console.log(gen.next()); // { value: 2, done: false }
+// console.log(gen.next()); // { done: true }
+//------------------------------------------------
+// Not actual code. A thought experiment
+// async function foo() {
+//   try {
+//       var val = await getMeAPromise();
+//       console.log(val);
+//   }
+//   catch(err) {
+//       console.log('Error: ', err.message);
+//   }
 // }
-// // output will be 0 1 2
-// ------------------------------------------------------
-// const
-// const foo = 123;
-// const foo = 123;
-// foo = 456; // ERROR: Left-hand side of an assignment expression
-// //cannot be a constant
-// const foo = { bar: 123 };
-// foo = { bar: 456 }; // ERROR : Left hand side of an assignment
-// //expression cannot be a constant
-// const foo = { bar: 123 };
-// foo.bar = 456; // Allowed!
-// console.log(foo); // { bar: 456
-// --------------------------------------------------------------
-// // Destructing
-// var rect = { x: 0, y: 10, width: 15, height: 20 };
-// // Destructuring assignment
-// var { x, y, width, height } = rect;
-// console.log(x, y, width, height); // 0,10,15,20
-// rect.x = 10;
-// ({ x, y, width, height } = rect); // assign to existing variables using outer parentheses
-// console.log(x, y, width, height); // 10,10,15,20
-// // Object Destructuring with rest
-// var { w, x, ...remaining } = { w: 1, x: 2, y: 3, z: 4 };
-// console.log(w, x, remaining); // 1, 2, {y:3,z:4}
-// // A common use case is also to ignore certain properties
-// // Example function
-// function goto(point2D: { x: number; y: number }) {
-//   // Imagine some code that might break
-//   // if you pass in an object
-//   // with more items than desired
-// }
-// // Some point you get from somewhere
-// const point3D = { x: 1, y: 2, z: 3 };
-// /** A nifty use of rest to remove extra properties */
-// const { z, ...point2D } = point3D;
-// console.log(point2D);
-// goto(point2D);
-// // Array Destructing
-// var x = 1, y = 2;
-// [x, y] = [y, x];
-// console.log(x, y); // 2,1
-// // Array Destructuring with rest
-// // You can pick up any number of elements from an array and get an array of the
-// // remaining elements using array destructuring with rest.
-// var [x, y, ...remaining2] = [1, 2, 3, 4];
-// console.log(x, y, remaining2); // 1, 2, [3,4]
-// var [x, , ...remaining3] = [1, 2, 3, 4];
-// console.log(x, remaining3); // 1, [3,4]
-//// ------------------------------------------------
-// // Spread operator
-//// Previously you would need to use Function.prototype.apply
-// function foo(x, y, z) {}
-// var args = [0, 1, 2];
-// console.log(args);
-// console.log(...args);
-// //foo(...args); // it doesn't work and i don't know why
-// // Destructing
-// var [x, y, ...remaining] = [1, 2, 3, 4];
-// console.log(x, y, remaining); // 1,2,[3,4]
-// // Array assignments.
-// // It allows an expanded version of an array into another array
-// var list = [1, 2];
-// list = [...list, 3, 4];
-// console.log(list); // [1,2,3,4]
-// var list = [1, 2];
-// list = [0, ...list, 4];
-// console.log(list); // [0,1,2,4]
-// //-----------------------------------------------------------
-// // Object Spread
-// const point2D = {x: 1, y: 2};
-// /** Create a new object by using all the point2D props along with z */
-// const point3D = {...point2D, z: 3};
-// // what comes first is 'overridden' by what comes later:
-// const point2D_1 = {x: 1, y: 2};
-// const anotherPoint3D = {x: 5, z: 4, ...point2D_1};
-// console.log(anotherPoint3D); // {x: 1, y: 2, z: 4}
-// const yetAnotherPoint3D = {...point2D_1, x: 5, z: 4}
-// console.log(yetAnotherPoint3D); // {x: 5, y: 2, z: 4}
-// --------------------------------------------------------
-// for...of
-// for...in for an array does not iterate over the array items. Instead it iterates over
-// the keys of the object passed in.
-// var someArray = [9, 2, 5];
-// for (var item in someArray) {
-//   console.log(item); // 0,1,2
-// }
-var someArray = [9, 2, 5];
-for (var _i = 0, someArray_1 = someArray; _i < someArray_1.length; _i++) {
-    var item2 = someArray_1[_i];
-    console.log(item2); // 9,2,5
+function delay(milliseconds, count) {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve(count);
+        }, milliseconds);
+    });
 }
-// var hello = "is it me you're looking for?";
-// for (var char of hello) {
-//   console.log(char); // is it me you're looking for?
-// }
+// async function always returns a Promise
+function dramaticWelcome() {
+    return __awaiter(this, void 0, void 0, function () {
+        var i, count;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    console.log("Hello");
+                    i = 0;
+                    _a.label = 1;
+                case 1:
+                    if (!(i < 5)) return [3 /*break*/, 4];
+                    return [4 /*yield*/, delay(500, i)];
+                case 2:
+                    count = _a.sent();
+                    console.log(count);
+                    _a.label = 3;
+                case 3:
+                    i++;
+                    return [3 /*break*/, 1];
+                case 4:
+                    console.log("World!");
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
--- TypeScript/deepDive.ts
+++ TypeScript/deepDive.ts
@@ -454,3 +454,81 @@ type StatusEnum = "Good" | "Bad" | "None";
 
 const status: StatusEnum = "Bad";
 // you get intellisense and you are just able to assign one of those values
+//--------------------------------------------------
+// //Promise
+// function iReturnPromiseAfter1Second(): Promise<string> {
+//   return new Promise(resolve => {
+//     setTimeout(() => resolve("Hello world!"), 1000);
+//   });
+// }
+
+// const promise = new Promise((resolve, reject) => {
+//   resolve(123);
+// });
+// promise
+//   .then(res => {
+//     // res is inferred to be of type `number`
+//     return iReturnPromiseAfter1Second();
+//   })
+//   .then(res => {
+//     // res is inferred to be of type `string`
+//     console.log(res); // Hello world!
+//   });
+//-------------------------------------------------------
+// Generators
+// function* infiniteSequence() {
+//   var i = 0;
+//   while (true) {
+//     yield i++;
+//   }
+// }
+
+// var iterator = infiniteSequence();
+// while (true) {
+//   console.log(iterator.next()); // { value: xxxx, done: false } forever and ever
+// }
+// // Of course if the iterator does end, you get the result
+// // of { done: true } as demonstrated below:
+// function* idMaker() {
+//   let index = 0;
+//   while (index < 3) yield index++;
+// }
+
+// let gen = idMaker();
+
+// console.log(gen.next()); // { value: 0, done: false }
+// console.log(gen.next()); // { value: 1, done: false }
+// console.log(gen.next()); // { value: 2, done: false }
+// console.log(gen.next()); // { done: true }
+//------------------------------------------------
+// Not actual code. A thought experiment
+// async function foo() {
+//   try {
+//       var val = await getMeAPromise();
+//       console.log(val);
+//   }
+//   catch(err) {
+//       console.log('Error: ', err.message);
+//   }
+// }
+
+// function delay(milliseconds: number, count: number): Promise<number> {
+//   return new Promise<number>(resolve => {
+//     setTimeout(() => {
+//       resolve(count);
+//     }, milliseconds);
+//   });
+// }
+
+// // async function always returns a Promise
+// async function dramaticWelcome(): Promise<void> {
+//   console.log("Hello");
+
+//   for (let i = 0; i < 5; i++) {
+//     // await is converting Promise<number> into number
+//     const count: number = await delay(500, i);
+//     console.log(count);
+//   }
+
+//   console.log("World!");
+// }
