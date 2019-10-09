3ab0f707ea8b462ef164753c52afadf335bf71e0
Saeid Pahlevan <s.pahlevan@fugro.com>
2019-10-10 07:58:45 +0800

    Typescript: added object

---
+++ TypeScript/object.ts
@@ -0,0 +1,25 @@
+// // ------------delete property---------------------
+// const gimli = {
+//   name: "Gimli",
+//   race: "dwarf",
+//   weapon: "axe",
+//   greet: function() {
+//     return `Hi, my name is ${this.name}!`;
+//   }
+// };
+// console.log(gimli);
+// delete gimli.weapon;
+// console.log(gimli);
+
+// ------------------------looping through-------------------------
+const gimli = {
+  name: "Gimli",
+  race: "dwarf",
+  weapon: "battle axe"
+};
+// iterate through properties of gimli
+for (let key in gimli) {
+  console.log(gimli[key]);
+}
+
+console.log(Object.keys(gimli));
